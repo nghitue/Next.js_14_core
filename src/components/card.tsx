2cf9e1b64e650ba0e13923ebb10a72a23ab85087
ghthuy <117986507+ghthuy@users.noreply.github.com>
2024-08-22 13:23:18 +0700

    up

---
+++ src/components/card.tsx
@@ -0,0 +1,13 @@
+const Card = ({ children }: { children: React.ReactNode }) => {
+    const cardStyle = {
+        padding: "100px",
+        margin: "10px",
+        border: "1px solid #ddd",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+    };
+    return <div style={cardStyle}>{children}</div>
+}
+export default Card;
